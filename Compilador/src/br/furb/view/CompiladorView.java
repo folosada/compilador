41751e912701479115bc4200d4410990e7101312
Matheus Waltrich <Matheus Waltrich@Matheus>
2017-03-13 22:46:06 -0300

    Alteração da tela do compilador.

--- Compilador/src/br/furb/view/CompiladorView.java
+++ Compilador/src/br/furb/view/CompiladorView.java
@@ -6,6 +6,12 @@
 package br.furb.view;
 
 import br.furb.lib.NumberedBorder;
+import com.sun.glass.events.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
 
 /**
  *
@@ -20,6 +26,88 @@ public class CompiladorView extends javax.swing.JFrame {
         initComponents();
         this.editorTA.setBorder(new NumberedBorder());
         this.setSize(914, 627);
+
+        //Atalho do botão novo
+        jBNovo.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK), "evento");
+        jBNovo.getActionMap().put("evento", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jBNovo.doClick();
+            }
+        });
+
+        //Atalho do botão abrir
+        jBAbrir.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK), "evento");
+        jBAbrir.getActionMap().put("evento", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jBAbrir.doClick();
+            }
+        });
+
+        //Atalho do botão salvar
+        jBSalvar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK), "evento");
+        jBSalvar.getActionMap().put("evento", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jBSalvar.doClick();
+            }
+        });
+
+        //Atalho do botão copiar
+        jBCopiar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK), "evento");
+        jBCopiar.getActionMap().put("evento", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jBCopiar.doClick();
+            }
+        });
+
+        //Atalho do botão colar
+        jBColar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK), "evento");
+        jBColar.getActionMap().put("evento", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jBColar.doClick();
+            }
+        });
+
+        //Atalho do botão recortar
+        jBRecortar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_DOWN_MASK), "evento");
+        jBRecortar.getActionMap().put("evento", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jBRecortar.doClick();
+            }
+        });
+
+        //Atalho do botão compilar
+        jBCompilar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_F8, 0), "evento");
+        jBCompilar.getActionMap().put("evento", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jBCompilar.doClick();
+            }
+        });
+
+        //Atalho do botão gerar código
+        jBGerCod.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_F9, 0), "evento");
+        jBGerCod.getActionMap().put("evento", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jBGerCod.doClick();
+            }
+        });
+
+        //Atalho do botão equipe
+        jBEquipe.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0), "evento");
+        jBEquipe.getActionMap().put("evento", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jBEquipe.doClick();
+            }
+        });
+
     }
 
     /**
@@ -35,6 +123,15 @@ public class CompiladorView extends javax.swing.JFrame {
         principalJP = new javax.swing.JPanel();
         toolBarJP = new javax.swing.JPanel();
         barraFerramentaTB = new javax.swing.JToolBar();
+        jBNovo = new javax.swing.JButton();
+        jBAbrir = new javax.swing.JButton();
+        jBSalvar = new javax.swing.JButton();
+        jBCopiar = new javax.swing.JButton();
+        jBColar = new javax.swing.JButton();
+        jBRecortar = new javax.swing.JButton();
+        jBCompilar = new javax.swing.JButton();
+        jBGerCod = new javax.swing.JButton();
+        jBEquipe = new javax.swing.JButton();
         editorJP = new javax.swing.JPanel();
         editorJSP = new javax.swing.JScrollPane();
         editorTA = new javax.swing.JTextArea();
@@ -60,6 +157,133 @@ public class CompiladorView extends javax.swing.JFrame {
         barraFerramentaTB.setMaximumSize(new java.awt.Dimension(2147483647, 2147483647));
         barraFerramentaTB.setMinimumSize(new java.awt.Dimension(144, 544));
         barraFerramentaTB.setPreferredSize(new java.awt.Dimension(144, 544));
+
+        jBNovo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/furb/view/text-document.png"))); // NOI18N
+        jBNovo.setText("novo [ctrl-n]");
+        jBNovo.setFocusable(false);
+        jBNovo.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jBNovo.setMaximumSize(new java.awt.Dimension(145, 62));
+        jBNovo.setMinimumSize(new java.awt.Dimension(145, 62));
+        jBNovo.setPreferredSize(new java.awt.Dimension(145, 62));
+        jBNovo.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jBNovo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBNovoActionPerformed(evt);
+            }
+        });
+        barraFerramentaTB.add(jBNovo);
+
+        jBAbrir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/furb/view/file-in-folder.png"))); // NOI18N
+        jBAbrir.setText("abrir [ctrl-o]");
+        jBAbrir.setFocusable(false);
+        jBAbrir.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jBAbrir.setMaximumSize(new java.awt.Dimension(145, 62));
+        jBAbrir.setMinimumSize(new java.awt.Dimension(145, 62));
+        jBAbrir.setPreferredSize(new java.awt.Dimension(145, 62));
+        jBAbrir.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        barraFerramentaTB.add(jBAbrir);
+
+        jBSalvar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/furb/view/diskette.png"))); // NOI18N
+        jBSalvar.setText("salvar [ctrl-s]");
+        jBSalvar.setToolTipText("");
+        jBSalvar.setFocusable(false);
+        jBSalvar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jBSalvar.setMaximumSize(new java.awt.Dimension(145, 62));
+        jBSalvar.setMinimumSize(new java.awt.Dimension(145, 62));
+        jBSalvar.setPreferredSize(new java.awt.Dimension(145, 62));
+        jBSalvar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        barraFerramentaTB.add(jBSalvar);
+
+        jBCopiar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/furb/view/text-documents.png"))); // NOI18N
+        jBCopiar.setText("copiar [ctrl-c]");
+        jBCopiar.setFocusable(false);
+        jBCopiar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jBCopiar.setMaximumSize(new java.awt.Dimension(145, 62));
+        jBCopiar.setMinimumSize(new java.awt.Dimension(145, 62));
+        jBCopiar.setPreferredSize(new java.awt.Dimension(145, 62));
+        jBCopiar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jBCopiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBCopiarActionPerformed(evt);
+            }
+        });
+        barraFerramentaTB.add(jBCopiar);
+
+        jBColar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/furb/view/paste-file.png"))); // NOI18N
+        jBColar.setText("colar [ctrl-v]");
+        jBColar.setFocusable(false);
+        jBColar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jBColar.setMaximumSize(new java.awt.Dimension(145, 62));
+        jBColar.setMinimumSize(new java.awt.Dimension(145, 62));
+        jBColar.setPreferredSize(new java.awt.Dimension(145, 62));
+        jBColar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jBColar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBColarActionPerformed(evt);
+            }
+        });
+        barraFerramentaTB.add(jBColar);
+
+        jBRecortar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/furb/view/icon.png"))); // NOI18N
+        jBRecortar.setText("recortar [ctrl-x]");
+        jBRecortar.setFocusable(false);
+        jBRecortar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jBRecortar.setMaximumSize(new java.awt.Dimension(145, 62));
+        jBRecortar.setMinimumSize(new java.awt.Dimension(145, 62));
+        jBRecortar.setPreferredSize(new java.awt.Dimension(145, 62));
+        jBRecortar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jBRecortar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBRecortarActionPerformed(evt);
+            }
+        });
+        barraFerramentaTB.add(jBRecortar);
+
+        jBCompilar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/furb/view/music-player-play.png"))); // NOI18N
+        jBCompilar.setText("compilar [F8]");
+        jBCompilar.setFocusable(false);
+        jBCompilar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jBCompilar.setMaximumSize(new java.awt.Dimension(145, 62));
+        jBCompilar.setMinimumSize(new java.awt.Dimension(145, 62));
+        jBCompilar.setPreferredSize(new java.awt.Dimension(145, 62));
+        jBCompilar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jBCompilar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBCompilarActionPerformed(evt);
+            }
+        });
+        barraFerramentaTB.add(jBCompilar);
+
+        jBGerCod.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/furb/view/handyman-tools.png"))); // NOI18N
+        jBGerCod.setText("gerar código [F9]");
+        jBGerCod.setFocusable(false);
+        jBGerCod.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jBGerCod.setMaximumSize(new java.awt.Dimension(145, 62));
+        jBGerCod.setMinimumSize(new java.awt.Dimension(145, 62));
+        jBGerCod.setPreferredSize(new java.awt.Dimension(145, 62));
+        jBGerCod.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jBGerCod.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBGerCodActionPerformed(evt);
+            }
+        });
+        barraFerramentaTB.add(jBGerCod);
+
+        jBEquipe.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/furb/view/team.png"))); // NOI18N
+        jBEquipe.setText("equipe [F1]");
+        jBEquipe.setFocusable(false);
+        jBEquipe.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jBEquipe.setMaximumSize(new java.awt.Dimension(145, 62));
+        jBEquipe.setMinimumSize(new java.awt.Dimension(145, 62));
+        jBEquipe.setPreferredSize(new java.awt.Dimension(145, 62));
+        jBEquipe.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jBEquipe.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBEquipeActionPerformed(evt);
+            }
+        });
+        barraFerramentaTB.add(jBEquipe);
+
         toolBarJP.add(barraFerramentaTB, java.awt.BorderLayout.EAST);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
@@ -143,6 +367,37 @@ public class CompiladorView extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+
+    private void jBEquipeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBEquipeActionPerformed
+        mensagemTA.setText("Equipe: Flávio Omar Losada, Gabriel da Silva Bernardi, Matheus Waltrich Da Silva");
+    }//GEN-LAST:event_jBEquipeActionPerformed
+
+    private void jBCompilarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBCompilarActionPerformed
+        mensagemTA.setText("Compilação de programas ainda não foi implementada.");
+    }//GEN-LAST:event_jBCompilarActionPerformed
+
+    private void jBGerCodActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBGerCodActionPerformed
+        mensagemTA.setText("geração de código ainda não foi implementada.");
+    }//GEN-LAST:event_jBGerCodActionPerformed
+
+    private void jBNovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBNovoActionPerformed
+        editorTA.setText("");
+        mensagemTA.setText("");
+        statusJL.setText("Não modificado");
+    }//GEN-LAST:event_jBNovoActionPerformed
+
+    private void jBCopiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBCopiarActionPerformed
+        editorTA.copy();
+    }//GEN-LAST:event_jBCopiarActionPerformed
+
+    private void jBColarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBColarActionPerformed
+        editorTA.paste();
+    }//GEN-LAST:event_jBColarActionPerformed
+
+    private void jBRecortarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBRecortarActionPerformed
+        editorTA.cut();
+    }//GEN-LAST:event_jBRecortarActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -183,6 +438,15 @@ public class CompiladorView extends javax.swing.JFrame {
     private javax.swing.JPanel editorJP;
     private javax.swing.JScrollPane editorJSP;
     private javax.swing.JTextArea editorTA;
+    private javax.swing.JButton jBAbrir;
+    private javax.swing.JButton jBColar;
+    private javax.swing.JButton jBCompilar;
+    private javax.swing.JButton jBCopiar;
+    private javax.swing.JButton jBEquipe;
+    private javax.swing.JButton jBGerCod;
+    private javax.swing.JButton jBNovo;
+    private javax.swing.JButton jBRecortar;
+    private javax.swing.JButton jBSalvar;
     private javax.swing.JPanel mensagemJP;
     private javax.swing.JScrollPane mensagemJSP;
     private javax.swing.JTextArea mensagemTA;
